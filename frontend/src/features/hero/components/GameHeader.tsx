84e9f4d6e243f74ef632f0d4393ebc95c8197e88
AndrShept <lolokos1986@gmail.com>
2026-04-12 15:49:37 +0300

    refactor: clean up unused imports and improve header component formatting

--- frontend/src/features/hero/components/GameHeader.tsx
+++ frontend/src/features/hero/components/GameHeader.tsx
@@ -5,7 +5,6 @@ import { useHero } from '@/features/hero/hooks/useHero';
 import { BackpackButton } from '@/features/item-container/components/BackpackButton';
 import { ContainerCapacityInfo } from '@/features/item-container/components/ContainerCapacityInfo';
 import { useHeroBackpack } from '@/features/item-container/hooks/useHeroBackpack';
-import { ShopItemCardButton } from '@/features/shop/components/ShopItemCardButton';
 import { SkillsPopover } from '@/features/skill/components/SkillsPopover';
 import { imageConfig } from '@/shared/config/image-config';
 import { Link, useLocation } from '@tanstack/react-router';
@@ -24,7 +23,7 @@ export const GameHeader = memo(() => {
   const { pathname } = useLocation();
   const isMobile = useMediaQuery('(max-width: 640px)');
   return (
-    <header className="bg-background/80  backdrop-blur-xs sticky top-0 z-50 mb-3 flex items-center justify-between border-b px-4  py-2">
+    <header className="bg-background/80 backdrop-blur-xs sticky top-0 z-50 mb-3 flex items-center justify-between border-b px-4 py-2">
       <section>
         <Link to="/">
           <GameIcon image={imageConfig.icon.ui.logo} />
@@ -35,7 +34,7 @@ export const GameHeader = memo(() => {
         <BackpackButton />
         <SkillsPopover />
         <GroupMenuButton />
-        {placeId && <ShopItemCardButton />}
+        {/* {placeId && <ShopItemCardButton />} */}
       </section>
       <section className="flex items-center gap-1 text-[15px]">
         {pathname === '/' && <LogOutButton />}
@@ -48,7 +47,9 @@ export const GameHeader = memo(() => {
           <span>{premiumCoins}</span>
         </div>
 
-        {!isMobile && <ContainerCapacityInfo iconSize='size-6' usedCapacity={backpack?.itemsInstance.length ?? 0} capacity={backpack?.capacity ?? 0} />}
+        {!isMobile && (
+          <ContainerCapacityInfo iconSize="size-6" usedCapacity={backpack?.itemsInstance.length ?? 0} capacity={backpack?.capacity ?? 0} />
+        )}
       </section>
     </header>
   );
--- frontend/src/features/shop/components/ShopItemCardButton.tsx
+++ frontend/src/features/shop/components/ShopItemCardButton.tsx
@@ -1,77 +1,77 @@
-import { CounterBadge } from '@/components/CounterBadge';
-import { GameIcon } from '@/components/GameIcon';
-import { GameItemImg } from '@/components/GameItemImg';
-import { Button } from '@/components/ui/button';
-import { ScrollArea } from '@/components/ui/scroll-area';
-import { Sheet, SheetClose, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
-import { imageConfig } from '@/shared/config/image-config';
-import { useShopItemStore } from '@/store/useShopItemStore';
-import { X } from 'lucide-react';
+// import { CounterBadge } from '@/components/CounterBadge';
+// import { GameIcon } from '@/components/GameIcon';
+// import { GameItemImg } from '@/components/GameItemImg';
+// import { Button } from '@/components/ui/button';
+// import { ScrollArea } from '@/components/ui/scroll-area';
+// import { Sheet, SheetClose, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+// import { imageConfig } from '@/shared/config/image-config';
+// import { useShopItemStore } from '@/store/useShopItemStore';
+// import { X } from 'lucide-react';
 
-import { ShopBuyButton } from './ShopBuyButton';
-import { TINT_COLOR } from '@/lib/config';
-import { useGameData } from '@/features/hero/hooks/useGameData';
-import { ColoredResourceType } from '@/shared/types';
+// import { ShopBuyButton } from './ShopBuyButton';
+// import { TINT_COLOR } from '@/lib/config';
+// import { useGameData } from '@/features/hero/hooks/useGameData';
+// import { ColoredResourceType } from '@/shared/types';
 
-export const ShopItemCardButton = () => {
-  const { isOpen, toggleIsOpen, items, getTotalPrice, clearAllItems, getTotalQuantity, decrement, increment } = useShopItemStore();
-  const totalPrice = getTotalPrice();
-  const totalQuantity = getTotalQuantity();
-  const {itemsTemplateById} = useGameData()
-  if (!totalQuantity) return;
-  return (
-    <Sheet open={isOpen} onOpenChange={toggleIsOpen} modal={false}>
-      <SheetTrigger asChild>
-        <Button variant={'outline'}  size="icon-lg" className="relative">
-          <GameIcon image={imageConfig.icon.ui['shop-cart']} />
-          <CounterBadge value={totalQuantity} />
-        </Button>
-      </SheetTrigger>
-      <SheetContent
-        onPointerDownOutside={(e) => e.preventDefault()}
-        onInteractOutside={(e) => e.preventDefault()}
-        className="w-50 p-1 md:w-full"
-      >
-        <SheetTitle className="hidden"></SheetTitle>
+// export const ShopItemCardButton = () => {
+//   const { isOpen, toggleIsOpen, items, getTotalPrice, clearAllItems, getTotalQuantity, decrement, increment } = useShopItemStore();
+//   const totalPrice = getTotalPrice();
+//   const totalQuantity = getTotalQuantity();
+//   const {itemsTemplateById} = useGameData()
+//   if (!totalQuantity) return;
+//   return (
+//     <Sheet open={isOpen} onOpenChange={toggleIsOpen} modal={false}>
+//       <SheetTrigger asChild>
+//         <Button variant={'outline'}  size="icon-lg" className="relative">
+//           <GameIcon image={imageConfig.icon.ui['shop-cart']} />
+//           <CounterBadge value={totalQuantity} />
+//         </Button>
+//       </SheetTrigger>
+//       <SheetContent
+//         onPointerDownOutside={(e) => e.preventDefault()}
+//         onInteractOutside={(e) => e.preventDefault()}
+//         className="w-50 p-1 md:w-full"
+//       >
+//         <SheetTitle className="hidden"></SheetTitle>
 
-        <h2 className="text-center text-xl">Buy Item</h2>
-        <ScrollArea className="h-full min-h-0">
-          <ul className="flex min-h-0 flex-col gap-0.5 md:px-3">
-            {items?.map((item) => (
-              <li className="flex flex-col items-center justify-between gap-1 md:flex-row" key={item.id}>
-                <div className="flex w-full flex-col items-center truncate md:flex-row md:gap-4">
-                  <div className="flex items-center gap-1 truncate">
-                    <GameItemImg tintColor={TINT_COLOR[itemsTemplateById[item.id].key as ColoredResourceType] } className="size-8" image={item.image} />
-                    <span className="truncate">{item.name}</span>
-                  </div>
+//         <h2 className="text-center text-xl">Buy Item</h2>
+//         <ScrollArea className="h-full min-h-0">
+//           <ul className="flex min-h-0 flex-col gap-0.5 md:px-3">
+//             {items?.map((item) => (
+//               <li className="flex flex-col items-center justify-between gap-1 md:flex-row" key={item.id}>
+//                 <div className="flex w-full flex-col items-center truncate md:flex-row md:gap-4">
+//                   <div className="flex items-center gap-1 truncate">
+//                     <GameItemImg tintColor={TINT_COLOR[itemsTemplateById[item.id].key as ColoredResourceType] } className="size-8" image={item.image} />
+//                     <span className="truncate">{item.name}</span>
+//                   </div>
 
-                  <span className="text-muted-foreground md:ml-auto">{item.quantity * item.price}</span>
-                </div>
-                <div className="flex gap-0.5">
-                  <Button onClick={() => decrement(item.id)} className="size-6" variant={'ghost'} size={'icon'}>
-                    -
-                  </Button>
-                  <span className="text-yellow-300">{item.quantity}</span>
+//                   <span className="text-muted-foreground md:ml-auto">{item.quantity * item.price}</span>
+//                 </div>
+//                 <div className="flex gap-0.5">
+//                   <Button onClick={() => decrement(item.id)} className="size-6" variant={'ghost'} size={'icon'}>
+//                     -
+//                   </Button>
+//                   <span className="text-yellow-300">{item.quantity}</span>
 
-                  <Button onClick={() => increment(item.id)} className="size-5.5" variant={'ghost'} size={'icon'}>
-                    +
-                  </Button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </ScrollArea>
-        <div className="flex w-full flex-col gap-1 px-3">
-          <div className="flex items-center justify-center gap-1">
-            <p className="w-fit md:text-xl">Total Cost: {totalPrice} </p>
-            <GameIcon className="size-6" image={imageConfig.icon.ui.gold} />
-          </div>
-          <ShopBuyButton items={items} />
-          <Button onClick={() => clearAllItems()} variant={'outline'} className="mt-1">
-            <X className="text-red-500/70 size-6" /> Clear All
-          </Button>
-        </div>
-      </SheetContent>
-    </Sheet>
-  );
-};
+//                   <Button onClick={() => increment(item.id)} className="size-5.5" variant={'ghost'} size={'icon'}>
+//                     +
+//                   </Button>
+//                 </div>
+//               </li>
+//             ))}
+//           </ul>
+//         </ScrollArea>
+//         <div className="flex w-full flex-col gap-1 px-3">
+//           <div className="flex items-center justify-center gap-1">
+//             <p className="w-fit md:text-xl">Total Cost: {totalPrice} </p>
+//             <GameIcon className="size-6" image={imageConfig.icon.ui.gold} />
+//           </div>
+//           <ShopBuyButton items={items} />
+//           <Button onClick={() => clearAllItems()} variant={'outline'} className="mt-1">
+//             <X className="text-red-500/70 size-6" /> Clear All
+//           </Button>
+//         </div>
+//       </SheetContent>
+//     </Sheet>
+//   );
+// };
